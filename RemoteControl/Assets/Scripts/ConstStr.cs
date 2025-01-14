bfea7c74f1a2def965b93a1a5f7cc3fd408345eb
guoyingkun2 <guo_yingkun@163.com>
2025-01-14 13:48:30 +0800

    多余物体隐藏

--- RemoteControl/Assets/Scripts/ConstStr.cs
+++ RemoteControl/Assets/Scripts/ConstStr.cs
@@ -146,7 +146,7 @@ public static class ConstStr
     public const float InitBucketWheelHeigh = 7.48f;
     public const string BucketWheelStackerReclaimerName = "1#斗轮堆取料机";
     public const string BucketWheelName = "2#斗轮取料机";
-    public const string Version = "V0.2.20250110-r";
+    public const string Version = "v0.1.20250114-m";
 }
 
 public enum Machine
