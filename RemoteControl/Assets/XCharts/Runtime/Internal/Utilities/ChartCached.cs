f37e453f0bd63f078e180a72ad7f0b407ef51860
guoyingkun2 <guo_yingkun@163.com>
2025-03-11 17:40:17 +0800

    数据格式修改

--- RemoteControl/Assets/XCharts/Runtime/Internal/Utilities/ChartCached.cs
+++ RemoteControl/Assets/XCharts/Runtime/Internal/Utilities/ChartCached.cs
@@ -66,7 +66,7 @@ namespace XCharts.Runtime
                     }
                     else
                     {
-                        s_NumberToStr[value][formatter] = value.ToString("F2");
+                        s_NumberToStr[value][formatter] = value.ToString();
                     }
                    
                 }
